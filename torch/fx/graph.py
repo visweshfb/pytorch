c40d246a3e6bd0266cad4aec9d329bc261e97331
James Reed <jamesreed@fb.com>
2020-09-17 13:43:14 -0700

    [FX] Make Graphs immutable after output is set
    ghstack-source-id: bc1a53bdb118007ad1005432842fd2b92bcd4a1e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44830

--- torch/fx/graph.py
+++ torch/fx/graph.py
@@ -67,8 +67,23 @@ def map_arg(a: Argument, fn: Callable[[Node], Argument]) -> Argument:
 
 class Graph:
     def __init__(self):
-        self.nodes : List[Node] = []
+        self._nodes : List[Node] = []
         self._used_names : Dict[str, int] = {}  # base name -> number
+
+    @property
+    def nodes(self):
+        return tuple(self._nodes)
+
+    def copy_graph(self):
+        """
+        Create a new graph containing the same nodes as `self` but without
+        `output` set. Thus, you can add new nodes to this graph copy.
+        """
+        g = Graph()
+        val_map = {}
+        for node in self._nodes:
+            val_map[node] = g.node_copy(node, lambda n : val_map[n])
+        return g
 
     def _mark_uses(self, a: Argument):
         def add_use(n: Node):
@@ -86,7 +101,7 @@ class Graph:
         self._mark_uses(args)
         self._mark_uses(kwargs)
         n = Node(self, name if name is not None else self._name(target), op, target, args, kwargs)
-        self.nodes.append(n)
+        self._nodes.append(n)
         return n
 
     # sugar for above when you know the op
@@ -161,7 +176,7 @@ class Graph:
     def python_code(self, root_module: str) -> Tuple[str, str, List[str]]:
         free_vars: List[str] = []
         body: List[str] = []
-        for node in self.nodes:
+        for node in self._nodes:
             if node.op == 'placeholder':
                 assert isinstance(node.target, str)
                 free_vars.append(node.target)
@@ -237,7 +252,7 @@ class Graph:
                        f'args = {format_arg(n.args)}, kwargs = {format_arg(n.kwargs)})'
 
 
-        node_strs = [format_node(node) for node in self.nodes]
+        node_strs = [format_node(node) for node in self._nodes]
         param_str = ', '.join(placeholder_names)
         s = f'graph({param_str}):'
         for node_str in node_strs:
